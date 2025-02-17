8b5c4f08e9f167b1bf2f701f6ea5770572f9ee44
Suzehva <suzeva@stanford.edu>
2025-02-16 22:11:21 -0800

    changed 1a data generation

---
+++ task1a/generate_data.py
@@ -0,0 +1,18 @@
+# TASK 1A:
+# LM input: "In 2029 there". We expect the LM to output "was", "is", or "will" as its
+# first token. We will sweep years. The first token will help us determine what an LM’s
+# internal time.
+
+TEMPLATES = ["In [[YEAR]] there "]
+START_YEAR = 1950
+END_YEAR = 2050
+
+def generate_year_data(start_year: int, end_year:int, templates):
+    with open('task1a.data', 'w', newline='',) as txtfile:
+        for template in templates:
+            for year in range(start_year, end_year + 1):
+                txtfile.write(template[:template.find("[[")] + str(year) + template[template.find("]]")+2:] + "\n")
+        
+
+if __name__ == "__main__":
+    generate_year_data(start_year=START_YEAR, end_year=END_YEAR, templates=TEMPLATES)
