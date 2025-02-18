5bb0e4906db0da784731636a04b689877752efe5
Aditi <aditijb@cs.stanford.edu>
2025-02-18 10:06:12 -0800

    updated run models

--- run_models.py
+++ run_models.py
@@ -93,7 +93,7 @@ class MultiModelManager:
 
 def main():
     model_ids = [
-        "meta-llama/Llama-3.2-1B",
+        # "meta-llama/Llama-3.2-1B",
         "allenai/OLMo-1B-hf",
         "google/gemma-2-2b",
     ]
