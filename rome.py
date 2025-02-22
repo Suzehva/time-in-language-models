d3113e1bff0501b6dd4c16214f1f2930f0ba60fc
Aditi <aditijb@cs.stanford.edu>
2025-02-22 15:50:34 -0800

    deleted dead code

--- rome.py
+++ rome.py
@@ -74,42 +74,13 @@ print(base)
 res = model(**inputs[0])
 
 # provided code, installed at /Users/aditi/Documents/cs224n-time/time/lib/python3.12/site-packages/pyvene/
-# distrib = embed_to_distrib(model, res.last_hidden_state, logits=False) # for encoder models 
 
-# distrib = embed_to_distrib(model, res.logits, logits=False) # for generation models # aditi added this 
-# def embed_to_distrib(model, embed, log=False, logits=False):
-#     """Convert an embedding to a distribution over the vocabulary"""
-#     if "gpt2" in model.config.architectures[0].lower():
-#         with torch.inference_mode():
-#             vocab = torch.matmul(embed, model.wte.weight.t())
-#             if logits:
-#                 return vocab
-#             return lsm(vocab) if log else sm(vocab)
-#     elif "llama" in model.config.architectures[0].lower():
-#         assert False, "Support for LLaMA is not here yet"
-
-
-# print(res.logits.shape) 
-
-# def top_vals(tokenizer, res, n=10, return_results=False):
-#     """Pretty print the top n values of a distribution over the vocabulary"""
-#     top_values, top_indices = torch.topk(res, n)
-#     ret = []
-#     for i, _ in enumerate(top_values):
-#         tok = format_token(tokenizer, top_indices[i].item())
-#         ret += [(tok, top_values[i].item())]
-#         if not return_results:
-#             print(f"{tok:<20} {top_values[i].item()}")
-#     if return_results:
-#         return ret
-
-
-# chat code
+# instead of using the provided distr function (doesn't work on tiny LLAMA or OLMO)
+#  distrib = embed_to_distrib(model, res.logits, logits=False) # for generation models # aditi added this 
 logits = res.logits[0, -1, :]  # Get the last token's logits (for "downtown")
-top_k = torch.topk(logits, 10)  # Get top 10 predictions
-top_tokens = [tokenizer.decode([idx]) for idx in top_k.indices.tolist()]
 distrib = sm(res.logits) 
 
-top_vals(tokenizer, distrib[0, -1], n=10)
-# print("Top predictions:", top_tokens)
+top_k = torch.topk(logits, 10)  # Get top 10 predictions
+top_tokens = [tokenizer.decode([idx]) for idx in top_k.indices.tolist()]
 
+top_vals(tokenizer, distrib[0, -1], n=10)
