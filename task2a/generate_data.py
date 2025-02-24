33173e9c7ec2fef152ef11de71f5c4baa8cc97b8
Aditi <aditijb@cs.stanford.edu>
2025-02-24 11:21:04 -0800

    created a new generate_data file

--- task2a/generate_data.py
+++ task2a/generate_data.py
@@ -12,14 +12,14 @@ PROMPTS = [
     ("The year is 1975. The current president of the United States is ", "Gerald Ford"),
     ("The year is 2015. The current president of the United States is ", "Barack Obama"),
     # Summer olympics 
-    ("The year is 1950. The next Summer Olympics will take place in the city of ", "Helsinki, Finland"),
-    ("The year is 1960. The current Summer Olympics will take place in the city of ", "Rome, Italy"),
-    ("The year is 1970. The next Summer Olympics will take place in the city of ", "Munich, West Germany"),
-    ("The year is 1979. The previous Summer Olympics had taken place in the city of ", "Montreal, Canada"),
-    ("The year is 1998. The next Summer Olympics will take place in the city of ", "Sydney, Australia, in 2000"),
-    ("The year is 2006. The next Summer Olympics will take place in the city of ", "Beijing, China, in 2008"),
-    ("The year is 2017. The previous Summer Olympics will take place in the city of ", "Rio de Janeiro, Brazil, in 2016"),
-    ("The year is 2028. The current Summer Olympics will take place in the city of ", "LA, USA"),
+    ("The year is 1950. The next Summer Olympics will take place in the city of ", "Helsinki Finland"),
+    ("The year is 1960. The current Summer Olympics will take place in the city of ", "Rome Italy"),
+    ("The year is 1970. The next Summer Olympics will take place in the city of ", "Munich West Germany"),
+    ("The year is 1979. The previous Summer Olympics had taken place in the city of ", "Montreal Canada"),
+    ("The year is 1998. The next Summer Olympics will take place in the city of ", "Sydney Australia in 2000"),
+    ("The year is 2006. The next Summer Olympics will take place in the city of ", "Beijing China in 2008"),
+    ("The year is 2017. The previous Summer Olympics will take place in the city of ", "Rio de Janeiro Brazil in 2016"),
+    ("The year is 2028. The current Summer Olympics will take place in the city of ", "LA USA"),
     # international PMs 
     ("The year is 1950. The current prime minister of the Netherlands is ", "Willem Drees"),
     ("The year is 1975. The current prime minister of the Netherlands is ", "Joop den Uyl"),
@@ -39,7 +39,7 @@ PROMPTS = [
 def generate_task2a(prompts):
     with open('task2a/task2a-with-solns.data', 'w', newline='',) as file:
         for text, value in prompts:
-            file.write(text + ",,, " + value + "\n")  ## ",,," seperated entries
+            file.write(text + ", " + value + "\n")  ## ",,," seperated entries
     with open('task2a/task2a.data', 'w', newline='',) as file:
         for text, value in prompts:
             file.write(text + "\n")  ## just the prompts
