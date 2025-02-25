c5090cec4f2495b3537517da97ff3e9e3af7c740
Aditi <aditijb@cs.stanford.edu>
2025-02-24 21:18:51 -0800

    got parts 1 and 2 running for olmo

--- rome_olmo-obsolete.py
+++ rome_olmo-obsolete.py
@@ -1,7 +1,8 @@
 # https://stanfordnlp.github.io/pyvene/tutorials/advanced_tutorials/Causal_Tracing.html
 
-
 import torch
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+
 import pandas as pd
 import numpy as np
 from pyvene import embed_to_distrib, top_vals, format_token
@@ -13,14 +14,16 @@ from pyvene import (
     ConstantSourceIntervention,
     LocalistRepresentationIntervention
 )
-from pyvene import create_olmo
-# from pyvene import create_gpt2
 
-# %config InlineBackend.figure_formats = ['svg'] 
+from torch import nn
+sm = nn.Softmax(dim=-1)
+lsm = nn.LogSoftmax(dim=-1)
+
+# %config InlineBackend.figure_formats = ['svg']
 import matplotlib as mpl
+
 mpl.rcParams['figure.figsize'] = (6, 4)  # Set default figure size
 mpl.rcParams['svg.fonttype'] = 'none'  # Keep text as text in SVGs
-
 from plotnine import (
     ggplot,
     geom_tile,
@@ -38,7 +41,7 @@ from plotnine.scales import scale_y_reverse, scale_fill_cmap
 from tqdm import tqdm
 
 titles={
-    "block_output": "single restored layer in OLMo 1B",
+    "block_output": "single restored layer in GPT2-XL",
     "mlp_activation": "center of interval of 10 patched mlp layer",
     "attention_output": "center of interval of 10 patched attn layer"
 }
@@ -49,32 +52,34 @@ colors={
     "attention_output": "Reds"
 } 
 
-##########################################
-print("## PART ONE: FACTUAL RECALL ##")
-##########################################
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
+# MODEL SETUP
 model_name = "allenai/OLMo-1B-hf"  # autoregressive model 
+config = AutoConfig.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-config, tokenizer, olmo = create_olmo(name=model_name) # create_gpt2(name="gpt2-xl")
-
-# config.output_hidden_states = True # aditi addition
-
-olmo.to(device)
+##########################################################################
+##  PART 1: FACTUAL RECALL 
+##########################################################################
+print("\nBEGINNING PART 1: FACTUAL RECALL\n")
 
 base = "The Space Needle is in downtown"
-inputs = [
-    tokenizer(base, return_tensors="pt").to(device),
-]
-# Aditi edit: say output_hidden_states=True manually. WHY ISN"T IT IN THE MODEL CONFIG??
-res = olmo(**inputs[0], output_hidden_states=True) 
-last_hidden_state = res.hidden_states[-1] # aditi addition
-distrib = embed_to_distrib(olmo, last_hidden_state, logits=False) # this function modified by Aditi in library
-top_vals(tokenizer, distrib[0][-1], n=10)
+inputs = [tokenizer(base, return_tensors="pt").to(device),]
+res = model(**inputs[0])
 
-##########################################
-print("## PART TWO: CORRUPTED RUN ##")
-##########################################
+# instead of using the provided distr function (doesn't work on tiny LLAMA or OLMO)
+#  distrib = embed_to_distrib(model, res.logits, logits=False) # for generation models # aditi added this 
+logits = res.logits[0, -1, :]  # Get the last token's logits (for "downtown")
+distrib = sm(res.logits) 
+
+top_vals(tokenizer, distrib[0, -1], n=10)
+
+##########################################################################
+##  PART 2: CORRUPTED RUN
+##########################################################################
+print("\nBEGINNING PART 2: CORRUPTED RUN\n")
 
 class NoiseIntervention(ConstantSourceIntervention, LocalistRepresentationIntervention):
     def __init__(self, embed_dim, **kwargs):
@@ -93,7 +98,6 @@ class NoiseIntervention(ConstantSourceIntervention, LocalistRepresentationInterv
     def __str__(self):
         return f"NoiseIntervention(embed_dim={self.embed_dim})"
 
-
 def corrupted_config(model_type):
     config = IntervenableConfig(
         model_type=model_type,
@@ -108,21 +112,45 @@ def corrupted_config(model_type):
     return config
 
 base = tokenizer("The Space Needle is in downtown", return_tensors="pt").to(device)
-config = corrupted_config(type(olmo))
-intervenable = IntervenableModel(config, olmo)
+config = corrupted_config(type(model))
+config.output_hidden_states = True # aditi addition
+
+intervenable = IntervenableModel(config, model)
+
+# IMPLEMENT THIS HACK IN THE PYVENE LIBRARY!!
+# my env is called time, so for me its under:
+#   /Users/aditi/Documents/cs224n-time/time/lib/python3.12/site-packages/pyvene
+# I modified line 1952 and 1953 in intervenable_base.py (imported file) like so:
+#    if 'output_hidden_states' in self.model.config:  # aditi addition 
+#        model_kwargs["output_hidden_states"] = True  # aditi addition 
+#    icounterfactual_outputs = self.model(**base, **model_kwargs)
+
 _, counterfactual_outputs = intervenable(
     base, unit_locations={"base": ([[[0, 1, 2, 3]]])}
 )
 
-# aditi addition. WHY IS THERE NO last_hidden_state?? prolly bc olmo model doesn't have it... how can i fix this?
-last_hidden_state = counterfactual_outputs.hidden_states[-1]   # aditi addition
-distrib = embed_to_distrib(olmo, last_hidden_state, logits=False)
+print("COUNTERFACTUAL OUTPUTS", counterfactual_outputs)
+# get the LAST hidden state. 
+last_hidden_state = counterfactual_outputs.hidden_states[-1]
+
+print( "last_hidden_state", last_hidden_state)
+print( "model", model)
+
+distrib = embed_to_distrib(model, last_hidden_state, logits=False)
+print("DISTRIB",distrib)
+
 top_vals(tokenizer, distrib[0][-1], n=10)
 
+##########################################################################
+##   PART 3: RESTORED RUN
+##########################################################################
+print("\nBEGINNING PART 3: RESTORED RUN\n")
 
-##########################################
-print("## PART THREE: RESTORED RUN ##")
-##########################################
+## CONTINUE NOTE:
+# Currently on part 3
+# the number of hidden layers in the model don't match up
+
+from pyvene.models.interventions import NoiseIntervention
 
 def restore_corrupted_with_interval_config(
     layer, stream="mlp_activation", window=10, num_layers=48):
@@ -145,23 +173,23 @@ def restore_corrupted_with_interval_config(
     return config
 
 # should finish within 1 min with a standard 12G GPU
-token = tokenizer.encode(" Seattle")[0]  # 16335
+token = tokenizer.encode(" Seattle")[0]  # 128000
 print(token)
-
-# test to make sure the tokens are not a constant lol :)
-# token = tokenizer.encode(" Aditi")[0]    # 2006
-# print(token)
 
 for stream in ["block_output", "mlp_activation", "attention_output"]:
     data = []
-    for layer_i in tqdm(range(olmo.config.n_layer)):
+    use_range = (model.config.num_hidden_layers) # for llama # aditi addition
+    # range(gpt.config.n_layer) for gpt
+    for layer_i in tqdm(range(min(use_range, 16))):  # TODO GET RID OF 16 bc its hacky !?
         for pos_i in range(7):
             config = restore_corrupted_with_interval_config(
                 layer_i, stream, 
                 window=1 if stream == "block_output" else 10
             )
+            config.output_hidden_states = True # aditi addition
+
             n_restores = len(config.representations) - 1
-            intervenable = IntervenableModel(config, olmo)
+            intervenable = IntervenableModel(config, model)
             _, counterfactual_outputs = intervenable(
                 base,
                 [None] + [base]*n_restores,
@@ -172,20 +200,18 @@ for stream in ["block_output", "mlp_activation", "attention_output"]:
                     )
                 },
             )
+            last_hidden_state = counterfactual_outputs.hidden_states[-1] # aditi addition
             distrib = embed_to_distrib(
-                olmo, counterfactual_outputs.last_hidden_state, logits=False
+                model, last_hidden_state, logits=False
             )
             prob = distrib[0][-1][token].detach().cpu().item()
             data.append({"layer": layer_i, "pos": pos_i, "prob": prob})
     df = pd.DataFrame(data)
+    import os  # aditi addition to create dir
+    os.makedirs("tutorial_data", exist_ok=True) # aditi addition to create dir
     df.to_csv(f"./tutorial_data/pyvene_rome_{stream}.csv")
 
-
-
-##########################################
-## PLOT
-##########################################
-
+# Graphing
 for stream in ["block_output", "mlp_activation", "attention_output"]:
     df = pd.read_csv(f"./tutorial_data/pyvene_rome_{stream}.csv")
     df["layer"] = df["layer"].astype(int)
@@ -210,4 +236,3 @@ for stream in ["block_output", "mlp_activation", "attention_output"]:
         plot, filename=f"./tutorial_data/pyvene_rome_{stream}.pdf", dpi=200
     )
     print(plot)
-
