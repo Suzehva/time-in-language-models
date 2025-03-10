160cd87b77853bf7162084ef00dd87b6d2fde0eb
Suzehva <suzeva@stanford.edu>
2025-03-09 20:28:13 -0700

    interchange interventions class works! Missing a few functions though

--- interchange_interventions.py
+++ interchange_interventions.py
@@ -87,25 +87,26 @@ class InterchangeIntervention:
         )
         return config
 
-    def intervene(self, base: str, sources: list[str]):
+    def intervene(self, base: str, sources: list[str], output_to_measure: list[str]):
         base_tokenized = self.tokenizer("The capital of Spain is", return_tensors="pt")
         sources_tokenized = [self.tokenizer("The capital of Italy is", return_tensors="pt")]
+        tokens = [self.tokenizer.encode(word) for word in output_to_measure]
 
-        data = []
-        #return
+        intervention_data = []
 
-        for layer_i in tqdm(range(self.model.config.num_hidden_layers)): # go through all hidden layers in model
+        for layer_i in tqdm(range(self.model.config.num_hidden_layers)): # looping over all hidden layers in model
             config = self.simple_position_config("mlp_output", layer_i)
             intervenable = IntervenableModel(config, self.model)
             for pos_i in range(len(base_tokenized.input_ids[0])): # looping over all the tokens in the base sentence
                 _, counterfactual_outputs = intervenable(
                     base_tokenized, sources_tokenized, {"sources->base": pos_i}
                 )
+                counterfactual_outputs.last_hidden_state = counterfactual_outputs.hidden_states[-1] # TODO: there must be a better way
                 distrib = embed_to_distrib(
                     self.model, counterfactual_outputs.last_hidden_state, logits=False
                 )
                 for token in tokens:
-                    data.append(
+                    intervention_data.append(
                         {
                             "token": format_token(self.tokenizer, token),
                             "prob": float(distrib[0][-1][token]),
@@ -115,36 +116,76 @@ class InterchangeIntervention:
                         }
                     )
 
-            config = self.simple_position_config(type(gpt), "attention_input", layer_i)
-            intervenable = IntervenableModel(config, gpt)
-            for pos_i in range(len(base.input_ids[0])):
+            config = self.simple_position_config("attention_input", layer_i)
+            intervenable = IntervenableModel(config, self.model)
+            for pos_i in range(len(base_tokenized.input_ids[0])):
                 _, counterfactual_outputs = intervenable(
-                    base, sources, {"sources->base": pos_i}
+                    base_tokenized, sources_tokenized, {"sources->base": pos_i}
                 )
+                counterfactual_outputs.last_hidden_state = counterfactual_outputs.hidden_states[-1] # TODO: there must be a better way
                 distrib = embed_to_distrib(
-                    gpt, counterfactual_outputs.last_hidden_state, logits=False
+                    self.model, counterfactual_outputs.last_hidden_state, logits=False
                 )
                 for token in tokens:
-                    data.append(
+                    intervention_data.append(
                         {
-                            "token": format_token(tokenizer, token),
+                            "token": format_token(self.tokenizer, token),
                             "prob": float(distrib[0][-1][token]),
                             "layer": f"a{layer_i}",
                             "pos": pos_i,
                             "type": "attention_input",
                         }
                     )
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(intervention_data)
+
+        os.makedirs(self.folder_path, exist_ok=True)
+        base_txt = base.replace(' ', '_')
+        output_prob_text = '(' + ''.join([s.replace(' ', '_') for s in output_to_measure]) + ')'
+        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filepath = "./"+self.folder_path+"/"+base_txt+output_prob_text+"_"+timestamp
+        print(f"saving interchange intervention data to {filepath}.pdf")
+        df.to_csv(filepath+".csv")
+        return df
+
+    def heatmap_plot(self, df, base: str, sources: list[str], output_to_measure: list[str]):
+        df["layer"] = df["layer"].astype("category")
+        df["token"] = df["token"].astype("category")
+        nodes = []
+        for l in range(self.model.config.num_hidden_layers - 1, -1, -1):
+            nodes.append(f"f{l}")
+            nodes.append(f"a{l}")
+        df["layer"] = pd.Categorical(df["layer"], categories=nodes[::-1], ordered=True)
+
+        plot_heat = (
+            ggplot(df)
+            + geom_tile(aes(x="pos", y="layer", fill="prob", color="prob"))
+            + facet_wrap("~token")
+            + theme(axis_text_x=element_text(rotation=90))
+        )
+        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        base_txt = base.replace(' ', '_')
+        output_prob_text = '(' + ''.join([s.replace(' ', '_') for s in output_to_measure]) + ')'
+        filepath = "./"+self.folder_path+"/"+base_txt+output_prob_text+"_"+timestamp
+        print(f"saving interchange intervention heatmap to {filepath}.pdf")
+        ggsave(
+            plot_heat, filename=filepath+".pdf", dpi=200 # write pdf graph # TODO: how to save as png??
+        )
+    
+    def bar_plot(self, df):
+        pass
+
 
 
 def main():
     base_prompt = "The capital of Spain is" # sentence where part of residual stream will be replaced
     source_prompts = ["The capital of Italy is"] # sentence from which we take the replacement
     interchange_intervention = InterchangeIntervention(model_id="allenai/OLMo-1B-hf")
+    output_to_measure = [" Madrid", " Rome"]
     #interchange_intervention.factual_recall(prompt=base_prompt)
     #for s_p in source_prompts:
     #    interchange_intervention.factual_recall(prompt=s_p)
-    interchange_intervention.intervene(base=base_prompt, sources=source_prompts)
+    results_df = interchange_intervention.intervene(base=base_prompt, sources=source_prompts, output_to_measure=output_to_measure)
+    interchange_intervention.heatmap_plot(df=results_df, base=base_prompt, sources=source_prompts, output_to_measure=output_to_measure)
 
 
 if __name__ == "__main__":
@@ -152,32 +193,7 @@ if __name__ == "__main__":
 
 # -----------
 
-#tokens = tokenizer.encode(" Madrid Rome")
 """
-
-df["layer"] = df["layer"].astype("category")
-df["token"] = df["token"].astype("category")
-nodes = []
-for l in range(gpt.config.n_layer - 1, -1, -1):
-    nodes.append(f"f{l}")
-    nodes.append(f"a{l}")
-df["layer"] = pd.Categorical(df["layer"], categories=nodes[::-1], ordered=True)
-
-plot_heat = (
-    ggplot(df)
-    + geom_tile(aes(x="pos", y="layer", fill="prob", color="prob"))
-    + facet_wrap("~token")
-    + theme(axis_text_x=element_text(rotation=90))
-)
-timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-filepath = "./"+folder_path+"/"+timestamp
-print(f"saving file to {filepath}.pdf")
-ggsave(
-    plot_heat, filename=filepath+".pdf", dpi=200 # write pdf graph # TODO: how to save as png??
-)
-
-# ------
-
 filtered = df
 filtered = filtered[filtered["pos"] == 4]
 plot_bar = (
